e354eb6d1f24aaf0724ce03cde83e95a0c95fcbb
ryoosukesaito <rshyp196_sai@yahoo.co.jp>
2022-10-07 02:09:48 -0700

    added heart

---
+++ script.js
@@ -0,0 +1,24 @@
+// var heart = document.getElementById("heart");
+
+// function toggle(){
+//   if(heart.classList.contains("far")){
+//     heart.classList.remove("far");
+//     heart.classList.add("fas");
+//   }else{
+//     heart.classList.remove("fas");
+//     heart.classList.add("far"); 
+//   }
+// };
+
+
+var btn1 = document.getElementById("btn1");
+
+function toggle(){
+    if(btn1.classList.contains("far")){
+        btn1.classList.remove("far");
+        btn1.classList.add("fas"); 
+    }else{
+        btn1.classList.remove("fas");
+        btn1.classList.add("far");
+    }};
+
